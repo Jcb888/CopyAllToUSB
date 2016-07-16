18744229bc75ac359a85d07df3350c01ef7d1e9d
jc <jc@PC_BUREAU>
2016-07-16 17:52:06 +0200

    Ajoutez des fichiers projet.

---
+++ CopyAllToUSB/FormMain.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CopyAllToUSB
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.txtBoxSourcePath = new System.Windows.Forms.TextBox();
+            this.txtBoxDestinationPath = new System.Windows.Forms.TextBox();
+            this.buttonExecuter = new System.Windows.Forms.Button();
+            this.buttonSource = new System.Windows.Forms.Button();
+            this.buttonDestination = new System.Windows.Forms.Button();
+            this.buttonSauveConfig = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelCopieEnCours = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxSourcePath
+            // 
+            this.txtBoxSourcePath.Location = new System.Drawing.Point(12, 21);
+            this.txtBoxSourcePath.Name = "txtBoxSourcePath";
+            this.txtBoxSourcePath.Size = new System.Drawing.Size(340, 20);
+            this.txtBoxSourcePath.TabIndex = 0;
+            // 
+            // txtBoxDestinationPath
+            // 
+            this.txtBoxDestinationPath.Location = new System.Drawing.Point(12, 61);
+            this.txtBoxDestinationPath.Name = "txtBoxDestinationPath";
+            this.txtBoxDestinationPath.Size = new System.Drawing.Size(340, 20);
+            this.txtBoxDestinationPath.TabIndex = 1;
+            // 
+            // buttonExecuter
+            // 
+            this.buttonExecuter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExecuter.Location = new System.Drawing.Point(12, 105);
+            this.buttonExecuter.Name = "buttonExecuter";
+            this.buttonExecuter.Size = new System.Drawing.Size(95, 23);
+            this.buttonExecuter.TabIndex = 2;
+            this.buttonExecuter.Text = "Exécuter";
+            this.toolTip2.SetToolTip(this.buttonExecuter, "Exécuter une sauvegarde");
+            this.buttonExecuter.UseVisualStyleBackColor = true;
+            this.buttonExecuter.Click += new System.EventHandler(this.buttonExecuter_Click);
+            // 
+            // buttonSource
+            // 
+            this.buttonSource.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonSource.FlatAppearance.BorderSize = 0;
+            this.buttonSource.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSource.Location = new System.Drawing.Point(358, 20);
+            this.buttonSource.Name = "buttonSource";
+            this.buttonSource.Size = new System.Drawing.Size(40, 20);
+            this.buttonSource.TabIndex = 3;
+            this.buttonSource.Text = "...";
+            this.buttonSource.UseVisualStyleBackColor = true;
+            this.buttonSource.Click += new System.EventHandler(this.buttonSource_Click);
+            // 
+            // buttonDestination
+            // 
+            this.buttonDestination.Location = new System.Drawing.Point(358, 60);
+            this.buttonDestination.Name = "buttonDestination";
+            this.buttonDestination.Size = new System.Drawing.Size(40, 20);
+            this.buttonDestination.TabIndex = 4;
+            this.buttonDestination.Text = "...";
+            this.buttonDestination.UseVisualStyleBackColor = true;
+            this.buttonDestination.Click += new System.EventHandler(this.buttonDestination_Click);
+            // 
+            // buttonSauveConfig
+            // 
+            this.buttonSauveConfig.Location = new System.Drawing.Point(157, 105);
+            this.buttonSauveConfig.Name = "buttonSauveConfig";
+            this.buttonSauveConfig.Size = new System.Drawing.Size(75, 23);
+            this.buttonSauveConfig.TabIndex = 5;
+            this.buttonSauveConfig.Text = "Enregistrer";
+            this.toolTip1.SetToolTip(this.buttonSauveConfig, "Pour enregistrer les paramétres actuels");
+            this.buttonSauveConfig.UseVisualStyleBackColor = true;
+            this.buttonSauveConfig.Click += new System.EventHandler(this.buttonSauveConfig_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CopyAllToUSB.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(223, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 29);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelCopieEnCours
+            // 
+            this.labelCopieEnCours.AutoSize = true;
+            this.labelCopieEnCours.Location = new System.Drawing.Point(13, 135);
+            this.labelCopieEnCours.Name = "labelCopieEnCours";
+            this.labelCopieEnCours.Size = new System.Drawing.Size(0, 13);
+            this.labelCopieEnCours.TabIndex = 7;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 197);
+            this.Controls.Add(this.labelCopieEnCours);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonSauveConfig);
+            this.Controls.Add(this.buttonDestination);
+            this.Controls.Add(this.buttonSource);
+            this.Controls.Add(this.buttonExecuter);
+            this.Controls.Add(this.txtBoxDestinationPath);
+            this.Controls.Add(this.txtBoxSourcePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Arterris USB Sauve";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxSourcePath;
+        private System.Windows.Forms.TextBox txtBoxDestinationPath;
+        private System.Windows.Forms.Button buttonExecuter;
+        private System.Windows.Forms.Button buttonSource;
+        private System.Windows.Forms.Button buttonDestination;
+        private System.Windows.Forms.Button buttonSauveConfig;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelCopieEnCours;
+    }
+}
+
---
+++ CopyAllToUSB/FormMain.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CopyAllToUSB
+{
+
+
+    public partial class FormMain : Form
+    {
+        configObject co = new configObject();
+
+        public FormMain()
+        {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();//pour récupérer les arguments de la ligne de commande
+
+            XmlSerializer xs = new XmlSerializer(typeof(configObject));//pour serialiser en XML la config (sauvegarde des paths src et dst)
+            using (StreamReader rd = new StreamReader("config.xml"))
+            {
+                co = xs.Deserialize(rd) as configObject;
+                this.txtBoxSourcePath.Text = co.strSourcePath;
+                this.txtBoxDestinationPath.Text = co.strDestinationPath;
+
+            }
+
+            // si aplli est ouverte avec param == hide on cache la feneter et on lance la copie
+            if (args[0] == "hide")
+            {
+                this.SetVisibleCore(false);
+                this.copyAll();
+            }
+
+        }
+        /// <summary>
+        /// Pour cacher la fenetre si le parametre "hide" est passe en argument
+        /// </summary>
+        /// <param name="value"> true affcihe la fenetre</param>
+        protected override void SetVisibleCore(bool value)
+        {
+            base.SetVisibleCore(value);
+        }
+
+        public void copyAll()
+        {
+            //On copy déja toute l'arborescense
+            foreach (string dirPath in Directory.GetDirectories(co.strSourcePath, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(co.strSourcePath, co.strDestinationPath));
+
+            //On copie tout les fichiers
+            foreach (string newPath in Directory.GetFiles(co.strSourcePath, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(co.strSourcePath, co.strDestinationPath), true);
+        }
+
+        /// <summary>
+        /// Pour sérialiser l'objet contenant strSourcePath et strDestinationPath dans le fichier XML config.xml 
+        /// </summary>
+        private void creatXML()
+        {
+            try
+            {
+
+                if (co.strDestinationPath == "")
+                {
+                    MessageBox.Show("Veuillez choisir une destination");
+                    return;
+                }
+
+                if (co.strSourcePath == "")
+                {
+                    MessageBox.Show("Veuillez choisir la source");
+                    return;
+
+                }
+
+                XmlSerializer xs = new XmlSerializer(typeof(configObject));
+                using (StreamWriter wr = new StreamWriter("config.xml"))
+                {
+                    xs.Serialize(wr, co);
+                }
+
+                MessageBox.Show("enregitrement des paramétres bien effectué \n\r" + "Source: " + co.strSourcePath + "\n\r" + "Destination: " + co.strDestinationPath);
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("erreur lors de la sauvegarde des paramétres : {0}", e.StackTrace.ToString());
+            }
+
+        }
+
+        private void buttonSource_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                System.Windows.Forms.MessageBox.Show("Files found: " + files[0] + " " + fbd.SelectedPath.ToString(), "Message");
+                co.strSourcePath = fbd.SelectedPath.ToString();
+                txtBoxSourcePath.Text = co.strSourcePath;
+                this.creatXML();
+            }
+        }
+
+        private void buttonExecuter_Click(object sender, EventArgs e)
+        {
+            lancerLaCopie();
+
+        }
+
+        private void lancerLaCopie()
+        {
+            try
+            {
+
+                foreach (string dirPath in Directory.GetDirectories(co.strSourcePath, "*", SearchOption.AllDirectories))
+                    Directory.CreateDirectory(dirPath.Replace(co.strSourcePath, co.strDestinationPath));
+
+                //Copy all the files & Replaces any files with the same name
+                foreach (string newPath in Directory.GetFiles(co.strSourcePath, "*.*", SearchOption.AllDirectories)) { 
+                    File.Copy(newPath, newPath.Replace(co.strSourcePath, co.strDestinationPath), true);
+                    labelCopieEnCours.Text = newPath;
+                }
+
+                MessageBox.Show("la sauvegarde c'est bien terminée");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erreur lors de la copie: " + e.StackTrace.ToString());
+            }
+        }
+
+        private void buttonDestination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+               // System.Windows.Forms.MessageBox.Show("Files found: " + files[0] + " " + fbd.SelectedPath.ToString(), "Message");
+                co.strDestinationPath = fbd.SelectedPath.ToString();
+                this.txtBoxDestinationPath.Text = co.strDestinationPath;
+                this.creatXML();
+            }
+        }
+
+        private void buttonSauveConfig_Click(object sender, EventArgs e)
+        {
+            this.creatXML();
+        }
+    }
+
+    public class configObject
+    {
+        public String strSourcePath { get; set; }
+        public String strDestinationPath { get; set; }
+
+    }
+
+
+}
